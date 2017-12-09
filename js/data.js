3dfcebb4b6145b22cead6eed1bdb06a931a2129a
yumusu <k@yumusu.com>
2017-12-09 13:29:26 +0800

    add siteNav popup

---
+++ js/data.js
@@ -0,0 +1,412 @@
+var MainHeaderLinks = {
+    siteNavLinks: {
+        themeMarket: {
+            title: "主题市场",
+            color: "#fc4200",
+            colorHover: "#f50",
+            width: '24%',
+            links: [
+                {
+                    name: "女装",
+                    modify: "hot"
+                },
+                {
+                    name: "男装",
+                    modify: ""
+                },
+                {
+                    name: "内衣",
+                    modify: ""
+                },
+                {
+                    name: "鞋靴",
+                    modify: ""
+                },
+                {
+                    name: "箱包",
+                    modify: ""
+                },
+                {
+                    name: "婴童",
+                    modify: ""
+                },
+                {
+                    name: "家电",
+                    modify: "new"
+                },
+                {
+                    name: "数码",
+                    modify: ""
+                },
+                {
+                    name: "手机",
+                    modify: ""
+                },
+                {
+                    name: "美妆",
+                    modify: ""
+                },
+                {
+                    name: "珠宝",
+                    modify: ""
+                },
+                {
+                    name: "眼镜",
+                    modify: ""
+                },
+                {
+                    name: "手表",
+                    modify: ""
+                },
+                {
+                    name: "运动",
+                    modify: ""
+                },
+                {
+                    name: "户外",
+                    modify: ""
+                },
+                {
+                    name: "乐器",
+                    modify: ""
+                },
+                {
+                    name: "游戏",
+                    modify: ""
+                },
+                {
+                    name: "动漫",
+                    modify: ""
+                },
+                {
+                    name: "影视",
+                    modify: ""
+                },
+                {
+                    name: "美食",
+                    modify: ""
+                },
+                {
+                    name: "鲜花",
+                    modify: ""
+                },
+                {
+                    name: "宠物",
+                    modify: ""
+                },
+                {
+                    name: "农资",
+                    modify: ""
+                },
+                {
+                    name: "房产",
+                    modify: ""
+                },
+                {
+                    name: "装修",
+                    modify: ""
+                },
+                {
+                    name: "建材",
+                    modify: ""
+                },
+                {
+                    name: "家居",
+                    modify: "hot"
+                },
+                {
+                    name: "百货",
+                    modify: ""
+                },
+                {
+                    name: "汽车",
+                    modify: ""
+                },
+                {
+                    name: "二手车",
+                    modify: ""
+                },
+                {
+                    name: "办公",
+                    modify: ""
+                },
+                {
+                    name: "企业购",
+                    modify: ""
+                },
+                {
+                    name: "定制",
+                    modify: ""
+                },
+                {
+                    name: "教育",
+                    modify: ""
+                },
+                {
+                    name: "卡券",
+                    modify: ""
+                },
+                {
+                    name: "本地",
+                    modify: ""
+                }
+            ]
+        },
+        featureMarket: {
+            title: "特色市场",
+            color: "#9fb838",
+            colorHover: "#a0b959",
+            width: '33%',
+            links: [
+                {
+                    name: "iFashion",
+                    modify: "new"
+                },
+                {
+                    name: "爱逛街",
+                    modify: ""
+                },
+                {
+                    name: "美妆秀",
+                    modify: ""
+                },
+                {
+                    name: "全球购",
+                    modify: "hot"
+                },
+                {
+                    name: "腔调",
+                    modify: ""
+                },
+                {
+                    name: "淘女郎",
+                    modify: ""
+                },
+                {
+                    name: "星店",
+                    modify: ""
+                },
+                {
+                    name: "极有家",
+                    modify: "hot"
+                },
+                {
+                    name: "特色中国",
+                    modify: ""
+                },
+                {
+                    name: "拍卖会",
+                    modify: ""
+                },
+                {
+                    name: "淘宝众筹",
+                    modify: ""
+                },
+                {
+                    name: "中国质造",
+                    modify: ""
+                },
+                {
+                    name: "飞猪",
+                    modify: ""
+                },
+                {
+                    name: "亲宝贝",
+                    modify: "new"
+                },
+                {
+                    name: "闲鱼",
+                    modify: ""
+                },
+                {
+                    name: "农资",
+                    modify: ""
+                },
+                {
+                    name: "天天特价",
+                    modify: "hot"
+                },
+                {
+                    name: "Outlets",
+                    modify: ""
+                },
+                {
+                    name: "俪人购",
+                    modify: ""
+                },
+                {
+                    name: "聚名品",
+                    modify: ""
+                },
+                {
+                    name: "淘抢购",
+                    modify: ""
+                },
+                {
+                    name: "全球精选",
+                    modify: ""
+                },
+                {
+                    name: "非常大牌",
+                    modify: ""
+                },
+                {
+                    name: "试用",
+                    modify: ""
+                },
+                {
+                    name: "量贩团",
+                    modify: ""
+                },
+                {
+                    name: "阿里翻译",
+                    modify: "new"
+                }
+            ]
+        },
+        aliApp: {
+            title: "阿里APP",
+            color: "#d75696",
+            colorHover: "#df61ab",
+            width: '33%',
+            links: [
+                {
+                    name: "淘宝",
+                    modify: "hot"
+                },
+                {
+                    name: "天猫",
+                    modify: ""
+                },
+                {
+                    name: "支付宝",
+                    modify: ""
+                },
+                {
+                    name: "聚划算",
+                    modify: ""
+                },
+                {
+                    name: "飞猪",
+                    modify: ""
+                },
+                {
+                    name: "蚂蚁聚宝",
+                    modify: "new"
+                },
+                {
+                    name: "旺信",
+                    modify: ""
+                },
+                {
+                    name: "闲鱼",
+                    modify: "hot"
+                },
+                {
+                    name: "阿里钱盾",
+                    modify: ""
+                },
+                {
+                    name: "钉钉",
+                    modify: "hot"
+                },
+                {
+                    name: "高德地图",
+                    modify: ""
+                },
+                {
+                    name: "点点虫",
+                    modify: ""
+                },
+                {
+                    name: "虾米音乐",
+                    modify: ""
+                },
+                {
+                    name: "淘票票",
+                    modify: ""
+                },
+                {
+                    name: "菜鸟裹裹",
+                    modify: "new"
+                },
+                {
+                    name: "爱逛街",
+                    modify: ""
+                },
+                {
+                    name: "拍卖会",
+                    modify: ""
+                },
+                {
+                    name: "阿里云",
+                    modify: ""
+                },
+                {
+                    name: "网商银行",
+                    modify: ""
+                },
+                {
+                    name: "阿里邮箱",
+                    modify: "new"
+                },
+                {
+                    name: "阿里众包",
+                    modify: ""
+                }
+            ]
+        },
+        specialTopic: {
+            title: "精彩推荐集",
+            color: "#2e9fdb",
+            colorHover: "#52a0e5",
+            width: '49%',
+            links: [
+                {
+                    name: "余额宝",
+                    modify: "hot"
+                },
+                {
+                    name: "大牌捡宝",
+                    modify: ""
+                },
+                {
+                    name: "淘公仔",
+                    modify: ""
+                },
+                {
+                    name: "浏览器",
+                    modify: ""
+                },
+                {
+                    name: "淘宝香港",
+                    modify: ""
+                },
+                {
+                    name: "淘宝台湾",
+                    modify: ""
+                },
+                {
+                    name: "淘宝全球",
+                    modify: ""
+                },
+                {
+                    name: "淘宝东南亚",
+                    modify: ""
+                },
+                {
+                    name: "闺蜜淘货",
+                    modify: ""
+                },
+                {
+                    name: "大众评审",
+                    modify: ""
+                },
+                {
+                    name: "淘工作",
+                    modify: "new"
+                }
+            ]
+        }
+    }
+}
--- js/mainHeader.js
+++ js/mainHeader.js
@@ -3,6 +3,8 @@ function MainHeader( obj ){
     for( let i in obj){
         this[i] = obj[i];
     }
+    this.MainHeaderLinks = MainHeaderLinks;
+    console.log(this.MainHeaderLinks);
     this.init();
 }
 
@@ -16,6 +18,12 @@ MainHeader.prototype = {
         mthis.dropPopupDisplay(mthis.headerMessage);
         mthis.dropPopupDisplay(mthis.myTaobao);
         mthis.dropPopupDisplay(mthis.collectionHeader);
+        mthis.dropPopupDisplay(mthis.vendorCenter);
+        mthis.dropPopupDisplay(mthis.serviceLink);
+        mthis.dropPopupDisplay(mthis.siteNav);
+
+        // 执行创建网站导航的方法
+        mthis.creatSiteNavPopup();
     },
     // 下拉弹出下拉框方法，接收参数：dom元素
     dropPopupDisplay: function( tParent ){
@@ -29,6 +37,49 @@ MainHeader.prototype = {
             tParent.css('background-color', '#f5f5f5');
             t.hide();
         })
+    },
+    creatSiteNavPopup: function(){
+        var mthis = this;
+        // 获取导航面板
+        var siteNavPanel = mthis.siteNav.find(".siteNavPanel");
+        var siteNavLinks = mthis.MainHeaderLinks.siteNavLinks;  // 网站导航数据
+        // 第一层循环，添加子面板
+        for( let i in siteNavLinks){
+            let linkGather = siteNavLinks[i];  // 单独的链接集合，是一个对象
+            let childPanel = $('<div>').addClass("childPanel");
+            let linkTitle = $('<h4>').html(linkGather.title).addClass('linkTitle').css('color', linkGather.color);
+            linkTitle.appendTo(childPanel);
+            // 第二层循环，添加单个链接
+            for( let j in linkGather.links){
+                let link = linkGather.links[j];
+                let linkLi = $('<li>',{})
+                                .css({
+                                    'width': linkGather.width
+                                });
+                let linkElement = $('<a>',{
+                    href: 'javascript:;'
+                })
+                    .html(link.name)
+                    .hover(function(){
+                        $(this).css({
+                            'color': '#fff',
+                            'background-color': linkGather.colorHover
+                        });
+                    },function(){
+                        $(this).css({
+                            'color': '#000',
+                            'background-color': '#fff'
+                        })
+                    });
+                if(link.modify){
+                    let linkI = $('<i>',{}).addClass(link.modify);
+                    linkI.appendTo(linkElement);
+                }
+                linkElement.appendTo(linkLi)
+                linkLi.appendTo(childPanel);
+            }
+            childPanel.appendTo(siteNavPanel);
+        }
     }
 }
 
@@ -36,7 +87,10 @@ var obj = {
     userInfo: $("#userInfo"),
     headerMessage: $("#headerMessage"),
     myTaobao: $("#myTaobao"),
-    collectionHeader: $("#collectionHeader")
+    collectionHeader: $("#collectionHeader"),
+    vendorCenter: $("#vendorCenter"),
+    serviceLink: $("#serviceLink"),
+    siteNav: $("#siteNav")
 }
 
 new MainHeader( obj );
