9654d280c133e6899f90f6537dd734922c9f7450
yumusu <k@yumusu.com>
2017-12-10 16:42:45 +0800

    cart heading search

--- js/mainHeader.js
+++ js/mainHeader.js
@@ -5,6 +5,8 @@ function MainHeader( obj ){
     }
     this.MainHeaderLinks = MainHeaderLinks;
     console.log(this.MainHeaderLinks);
+    // 搜索类型列表
+    this.searchTypeValue = ['宝贝', '天猫', '店铺'];
     this.init();
 }
 
@@ -24,6 +26,12 @@ MainHeader.prototype = {
 
         // 执行创建网站导航的方法
         mthis.creatSiteNavPopup();
+        // 创建搜索类型列表
+        mthis.createSearchTypeList();
+
+        // 搜索框聚焦
+        mthis.mainHeadingSearchInput.focus();
+        mthis.searchInputEvent();
     },
     // 下拉弹出下拉框方法，接收参数：dom元素
     dropPopupDisplay: function( tParent ){
@@ -38,6 +46,7 @@ MainHeader.prototype = {
             t.hide();
         })
     },
+    // 创建网站导航
     creatSiteNavPopup: function(){
         var mthis = this;
         // 获取导航面板
@@ -80,6 +89,60 @@ MainHeader.prototype = {
             }
             childPanel.appendTo(siteNavPanel);
         }
+    },
+    // 创建搜索类型列表
+    createSearchTypeList: function(){
+        var mthis = this;
+        // 先清空ul内容
+        mthis.searchTypeUl.html('');
+        var searchTypeValueL = mthis.searchTypeValue.length;
+        for(let i = 0; i < searchTypeValueL; i++){
+            var searchTypeLi = $('<li>', { 
+                                    'class': (i == 0)
+                                                ? 'searchTypeLi selected' 
+                                                : 'searchTypeLi',
+                                    'data-index': i
+                                });
+            var searchTypeSpan = $('<a>',{ 'href': 'javascript:;'}).html( mthis.searchTypeValue[i] );
+            var searchTypeArrow = $('<i>',{ 'class': 'arrow'});
+            searchTypeSpan.appendTo(searchTypeLi);
+            searchTypeArrow.appendTo(searchTypeLi);
+
+            searchTypeLi.appendTo(mthis.searchTypeUl);
+        }
+
+        mthis.searchTypeListEvent();
+    },
+    // 给搜索类型列表添加点击事件
+    searchTypeListEvent: function(){
+        var mthis = this;
+        var searchTypeList = mthis.searchTypeUl.find('.searchTypeLi');
+        searchTypeList.on('click', function(){
+            var e = $(this);
+            mthis.searchTypeValue.splice(e.attr('data-index'), 1);  // 删除选中的数组元素
+            mthis.searchTypeValue.unshift(e.text());                // 然后添加到开头
+            console.log(mthis.searchTypeValue);
+
+            mthis.createSearchTypeList();
+        })
+    },
+    // 输入框聚焦和失去焦点方法
+    searchInputEvent: function(){
+        var mthis = this;
+        var searchIcon = mthis.mainHeadingSearchInput.siblings('.searchIcon');
+        mthis.mainHeadingSearchInput
+            .on({
+                'focus': function(){
+                    searchIcon.hide();
+                },
+                'blur': function(){
+                    if( $(this).prop('value').trim() ){
+                        return;
+                    }else{
+                        searchIcon.show();
+                    }
+                }
+            })
     }
 }
 
@@ -90,7 +153,10 @@ var obj = {
     collectionHeader: $("#collectionHeader"),
     vendorCenter: $("#vendorCenter"),
     serviceLink: $("#serviceLink"),
-    siteNav: $("#siteNav")
+    siteNav: $("#siteNav"),
+    // 搜索类型的ul容器
+    searchTypeUl: $("#searchTypeUl"),
+    mainHeadingSearchInput: $("#mainHeadingSearchInput")
 }
 
 new MainHeader( obj );
